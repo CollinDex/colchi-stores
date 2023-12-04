a8346370a8a60a040154046a5a65091d24711df3
CollinDex <obetscollins@gmail.com>
2023-12-04 16:01:10 +0100

    added signIn component as well as firebase utils and brought in firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/routes/home/home.component'
-import Navigation from './components/routes/navigation/navigation.component'; 
-
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'; 
+import SignIn  from './routes/sign-in/sign-in.component';
 const Shop = () => {
   return <h1>I dey for shop</h1>;
 };
@@ -10,8 +10,9 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation/>}>
-      <Route index element={<Home/>}/>
-        <Route path='/shop' element={<Shop/>}/> 
+        <Route index element={<Home/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/> 
       </Route>
     </Routes>
   );
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,71 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { 
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+ } from 'firebase/auth'
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOLzH5NTeYD_DKvQ6iOsVk-7Cvwp_5D5w",
+  authDomain: "colchi-clothing-db.firebaseapp.com",
+  projectId: "colchi-clothing-db",
+  storageBucket: "colchi-clothing-db.appspot.com",
+  messagingSenderId: "800131861362",
+  appId: "1:800131861362:web:2ed39b00005d701fdbabeb"
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const firebaseApp = initializeApp(firebaseConfig);
+
+//Initialize Provider
+const provider = new GoogleAuthProvider();
+
+//Always prompt every user to select an account
+provider.setCustomParameters({
+    prompt: 'select_account'    
+});
+
+export const auth = getAuth(); 
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider); 
+
+//Initialize firestore database
+export const db = getFirestore(); 
+
+//Create user data from Auth and add to Firestore Database
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    //Check if user data exists.
+    //If user does not exist, set the document with the data from userAuth in my collection
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef,{
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('Error Creating User', error.message);
+        }
+    }
+
+    //If user data exists, return useDocRef
+    return userDocRef;
+} 
