7db68bdab75ea47d2231f9b231d788baa2773d2c
CollinDex <obetscollins@gmail.com>
2023-12-17 17:44:18 +0100

    added in our addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,46 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+    // Find if cartItems contains productToAdd
+    const existingCartItem = cartItems.find( 
+        (cartItem) => cartItem.id === productToAdd.id
+    );
+
+    //If found, increment quantity
+    if (existingCartItem){
+        return cartItems.map((cartItem) =>
+            cartItem.id === productToAdd.id
+                ? {...cartItem, quantity: cartItem.quantity + 1} 
+                : cartItem
+        );
+    }  
+
+    //return new array with modified cartItems and the new cart item
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+}
 
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
+    cartCount: 0
 });
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};
+    const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => { //Updates the total count of items in the cart to display on the cart icon
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        setCartCount(newCartCount);
+    },[cartItems]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems,productToAdd));
+    }
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
