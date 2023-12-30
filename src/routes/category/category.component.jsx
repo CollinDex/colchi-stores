3bc64392072bbcc79abb6626bdf0b5bd995d7e21
CollinDex <obetscollins@gmail.com>
2023-12-30 16:42:40 +0100

    Fixed Clashing Styles

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { CategoriesContext } from '../../contexts/categories.context';
@@ -18,11 +18,14 @@ const Category = () => {
     },[category, categoriesMap]);
 
     return (
-        <div className='category-container'>
-            {products &&
-                products.map((product) => <ProductCard key={product.id} product={product}/>)
-            }
-        </div>
+        <Fragment>
+            <h2 className='title'>{category}</h2>
+            <div className='products-container'>
+                {products &&
+                    products.map((product) => <ProductCard key={product.id} product={product}/>)
+                }
+            </div>
+        </Fragment>
     );
 }
 
