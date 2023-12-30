b914c687a941d721da2ed4cb4de5bb9876b33de9
CollinDex <obetscollins@gmail.com>
2023-12-30 12:57:02 +0100

    Fixed Responsiveness for Category Preview Page

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -7,12 +7,11 @@ import './category-preview.styles.scss'
 const CategoryPreview = ({title, products}) => {
     return (
         <div className='category-preview-container'>
-            <h2>
+            
             <Link to= {`/shop/${title}`}>
                 <span className='title'>{title.toUpperCase()}</span>
             </Link> 
-                
-            </h2>
+
             <div className='preview'>
                 {   //Filter out first four items of each product category then render it using the ProductCard component
                     products
