fd2f54101002e10a8dabb42c59ccbb24c692494d
CollinDex <obetscollins@gmail.com>
2024-03-05 00:11:11 +0100

    Added User Reducer

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,12 @@
 import { Fragment, useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -14,9 +15,7 @@ import { NavigationContainer, LogoContainer, NavLinksContainer, NavLink  } from 
 
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
-    //console.log(currentUser);
-
+    const currentUser = useSelector(selectCurrentUser);
     const {isCartOpen} = useContext(CartContext); //Stores true or false for if the cart is open.
 
     const signOutHandler = async () => {
