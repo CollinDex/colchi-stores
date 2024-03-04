839ea43c80190b7eeab65975df968181ed6fc553
CollinDex <obetscollins@gmail.com>
2024-03-04 22:58:12 +0100

    About to downgrade react-redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,35 +1,11 @@
-import { createContext, useState, useEffect, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null,
 });
-
-export const USER_ACTION_TYPES = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER'
-};
-
-const userReducer = (state, action) => {
-    console.log('Dispatched User Reducer');
-    console.log(action);
-    const { type, payload } = action;
-
-    switch(type) {
-        case USER_ACTION_TYPES.SET_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: payload
-            }
-        default:
-            throw new Error(`Unhandled type ${type} in userReducer`);
-    }
-};
-
-const INITIAL_STATE = {
-    currentUser: null
-};
 
 export const UserProvider = ({ children }) => {
     //Using useState
